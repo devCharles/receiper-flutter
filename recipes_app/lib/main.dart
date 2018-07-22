0a29723b345ea00b755bf731ea9701c9e0f0919b
Gil Rodarte <gilrodar@gmail.com>
2018-07-22 18:19:45 -0500

    Template de la app

---
+++ recipes_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primaryColor: Colors.white),
+    title: 'Recipes',
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Recipes"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+        onPressed: () => debugPrint("Quiere añadir"),
+      )
+    );
+  }
+}
