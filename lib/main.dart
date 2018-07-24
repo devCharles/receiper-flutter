f6f0fcbdc6f17f3b22308bc3063d3ffedfa4f8dd
josecaos <jazzvoon@gmail.com>
2018-07-23 21:54:56 -0500

    Codigo base compilando

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         centerTitle: true,
         title: Text("Recipes"),
       ),
+      body: Register(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.white,
         child: Icon(
@@ -28,3 +29,116 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final TextEditingController _userController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+
+        title: Text("Login"),
+        backgroundColor: Colors.black12,
+      ),
+
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+          alignment: Alignment.topCenter,
+          child: Column(
+              children: <Widget>[
+//                Image.asset("images/caos.jpg",
+//                    height: 60.0,
+//                    width: 60.0,
+//                    color: Colors.purple
+//                ),
+
+//              Form
+                Container(
+                    height: 200.0,
+                    width: 320.0,
+                    color: Colors.white,
+
+                    child: Column(
+                        children: <Widget>[
+                          TextField(
+                            controller: _userController,
+                            decoration: InputDecoration(
+                                hintText: "User name", icon: Icon(Icons.person)
+                            ),
+                          )
+                        ]
+                    )
+
+                ),
+              ])
+      ),
+    );
+  }
+}
+
+
+//
+//
+//
+//
+//
+////clases para agregar receta
+//class RegisterRecipe extends MyApp {
+//
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+////    return Container(
+////      child: Row(
+////      children: <Widget>[
+////          Text("AYY weeeyyyy!")
+////      ],
+////        )
+////      );
+//
+//
+//  return Container(
+//      alignment: Alignment.topCenter,
+//      child: Column(
+//          children: <Widget>[
+//            Image.asset("images/caos.jpg",
+//                height: 60.0,
+//                width: 60.0,
+//                color: Colors.purple
+//            ),
+//
+////              Form
+//            Container(
+////                heght: 200.0,
+//                width: 320.0,
+//                color: Colors.white,
+//
+//                child: Column(
+//                    children: <Widget>[
+//                      TextField(
+//                        controller: _userController,
+//                        decoration: InputDecoration(
+//                            hintText: "User name", icon: Icon(Icons.person)
+//                        ),
+//                      )
+//                    ]
+//                )
+//
+//            ),
+//          ])
+//  ),
+//
+//
+//  }
+//}
