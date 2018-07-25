e9fbeafb1447c040bac471ce5a6944c3e529666e
josecaos <jazzvoon@gmail.com>
2018-07-25 17:37:49 -0500

    Layout listo, me falta un chingo :/

--- lib/main.dart
+++ lib/main.dart
@@ -66,9 +66,10 @@ class _RegisterState extends State<Register> {
         body: ListView(
           padding: EdgeInsets.all(16.0),
           children: <Widget>[
+
 //            Nombre
             Container(
-            color: Colors.lime,
+            color: Colors.white,
             padding: EdgeInsets.all(16.0),
             child: TextField(
 //            onChanged: (string) => debugPrint("Recipe Name"),
@@ -83,62 +84,81 @@ class _RegisterState extends State<Register> {
               hintText: "Recipe Name",
               icon: Icon(Icons.fastfood),
               fillColor: Colors.black26
-              )
+              ),
             )
             ),
 
-//            ingredientes
-            Container(
-              height: 250.0,
-                color: Colors.white,
-                padding: EdgeInsets.all(16.0),
-//                child: ListView(
-                  child:
+//          ingredientes
 
-                    ListView.builder(
-                      padding: EdgeInsets.all(16.0),
-                      itemExtent: 40.0,
-                      itemBuilder: (BuildContext context, int index) {
-
-                        return Text('Ingredient $index',
-                        style: TextStyle(
-                            color: Colors.black
-                          ),
-                        );
-
-                        },
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(16.0),
+                    child: RaisedButton(
+                      onPressed: () => debugPrint("Call modal window"),
+                      color: Colors.black26,
+                      splashColor:  Colors.blue,
+                      child: Text("Add Ingredients")
                     )
+                ),
+Card(
+
+  child:
+
+Container(
+height: 200.0,
+ child:           ListView.builder(
+                padding: EdgeInsets.all(16.0),
+                itemExtent: 32.0,
+                itemCount: 8,
+                itemBuilder: (BuildContext context, int index) {
+
+                  return Text('Ingredient $index',
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                  );
+                }
+            ),
+      ),
 
 
+),
 
-//                children: TextField(
-////            onChanged: (string) => debugPrint("Ingredients list"),
-//                    autofocus: false,
-//                    controller: _userController,
-//                    decoration: InputDecoration(
-//                        border: OutlineInputBorder(
-//                            borderSide: BorderSide(
-//                                color: Colors.white70
-//                            )
-//                        ),
-//                        hintText: "Ingredients",
-//                        icon: Icon(Icons.fastfood),
-//                        fillColor: Colors.black26
-//                    )
-//                )
 
-//              )
+//instrucciones
+            Container(
+//                height: 320.0,
+                color: Colors.white30,
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+//            onChanged: (string) => debugPrint("Recipe Name"),
+                  keyboardType: TextInputType.multiline,
+//                  maxLines:,
+                  autofocus: false,
+                  controller: _userController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.white70
+                          )
+                      ),
+                      hintText: "Recipe Instructions",
+                      icon: Icon(Icons.edit),
+                      fillColor: Colors.black26
+                  ),
+                )
             ),
 
 //            boton imagen
             Container(
-                color: Colors.greenAccent,
+                color: Colors.white,
                 padding: EdgeInsets.all(16.0),
                 child: RaisedButton(
-                  onPressed: () => debugPrint("Image selection"),
-                  color: Colors.purple,
-                  splashColor:  Colors.lightGreenAccent,
+                  onPressed: () => debugPrint("Select Recipe Image"),
+                  color: Colors.black26,
+                  splashColor:  Colors.blue,
                   child: Text("Select Image"),
+
                 )
             ),
 
@@ -148,91 +168,3 @@ class _RegisterState extends State<Register> {
 
   }
 }
-
-
-
-//class _RegisterState extends State<Register> {
-//
-//  final TextEditingController _userController = TextEditingController();
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//
-//      appBar: AppBar(
-//        centerTitle: true,
-//        title: Text("Register your Recipe",
-//          style: TextStyle(
-//              color: Colors.white70,
-//              fontSize: 14.0
-//          ),
-//        ),
-//        backgroundColor: Colors.white30,
-//      ),
-//
-//      backgroundColor: Colors.white70,
-//      body: Container(
-//          alignment: Alignment.topCenter,
-//          child: Column(
-//              children: <Widget>[
-////              Form
-//                Container(
-//                    height: 220.0,
-//                    width: 320.0,
-//                    padding: EdgeInsets.only(top:32.0),
-//                    color: Colors.black26,
-//                    child: Column(
-//                      mainAxisAlignment: MainAxisAlignment.,
-//                        children: <Widget>[
-//                          TextField(
-//                            autofocus: true,
-//                            controller: _userController,
-//                            decoration: InputDecoration(
-//                                hintText: "Recipe Name",
-//                                icon: Icon(Icons.fastfood),
-//                                fillColor: Colors.black26
-//                                )
-//                              ),
-//
-//                          TextField(
-//                              autofocus: false,
-//                              controller: _userController,
-//                              decoration: InputDecoration(
-//                                  hintText: "Recipe Ingredients",
-//                                  icon: Icon(Icons.fastfood),
-//                                  fillColor: Colors.black26
-//                              )
-//                          ),
-//
-//                          TextField(
-//                              autofocus: false,
-//                              controller: _userController,
-//                              decoration: InputDecoration(
-//                                  hintText: "Recipe Instructions",
-//                                  icon: Icon(Icons.fastfood),
-//                                  fillColor: Colors.black26
-//                              )
-//                          ),
-//
-////
-////                          new Center(
-////                            child:
-//                            Container(
-//                              margin: const EdgeInsets.all(10.0),
-//                              color: const Color(0xFF00FF00),
-//                              width: 88.0,
-//                              height: 48.0,
-//                            ),
-////                          )
-////
-//
-//                            ],
-//                          ),
-//                        )
-//                      ]
-//                    )
-//                  )
-//                );
-//
-//  }
-//}
