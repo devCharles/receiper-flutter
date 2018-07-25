6ac92d40095b308108f9e2b003f76b4bd7f42522
josecaos <jazzvoon@gmail.com>
2018-07-24 19:07:56 -0500

    Estructura mas parecida + paquete para imagendes + edicion 'keys' para entrar a la biblioteca en IOS sobre info.plist

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+//
 
 void main() {
   runApp(MaterialApp(
@@ -32,11 +34,15 @@ class MyApp extends StatelessWidget {
 
 
 
+//
+//Clases registro
 class Register extends StatefulWidget {
   @override
   _RegisterState createState() => _RegisterState();
 }
 
+
+//
 class _RegisterState extends State<Register> {
 
   final TextEditingController _userController = TextEditingController();
@@ -45,100 +51,164 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Register your Recipe",
+            style: TextStyle(
+                color: Colors.white70,
+                fontSize: 14.0
+            ),
+          ),
+          backgroundColor: Colors.white30,
+        ),
 
-        title: Text("Login"),
-        backgroundColor: Colors.black12,
-      ),
+        backgroundColor: Colors.white70,
+        body: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            Container(
+            color: Colors.lime,
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+//            onChanged: (string) => debugPrint("Recipe Name"),
+            autofocus: false,
+            controller: _userController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white70
+                )
+              ),
+              hintText: "Recipe Name",
+              icon: Icon(Icons.fastfood),
+              fillColor: Colors.black26
+              )
+            )
+            ),
 
-      backgroundColor: Colors.blueGrey,
-      body: Container(
-          alignment: Alignment.topCenter,
-          child: Column(
-              children: <Widget>[
-//                Image.asset("images/caos.jpg",
-//                    height: 60.0,
-//                    width: 60.0,
-//                    color: Colors.purple
-//                ),
-
-//              Form
-                Container(
-                    height: 200.0,
-                    width: 320.0,
-                    color: Colors.white,
-
-                    child: Column(
-                        children: <Widget>[
-                          TextField(
-                            controller: _userController,
-                            decoration: InputDecoration(
-                                hintText: "User name", icon: Icon(Icons.person)
-                            ),
-                          )
-                        ]
+            Container(
+              height: 250.0,
+                color: Colors.orangeAccent,
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+//            onChanged: (string) => debugPrint("Ingredients list"),
+                    autofocus: false,
+                    controller: _userController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.white70
+                            )
+                        ),
+                        hintText: "Ingredients",
+                        icon: Icon(Icons.fastfood),
+                        fillColor: Colors.black26
                     )
+                )
+            ),
 
-                ),
-              ])
-      ),
+            Container(
+                color: Colors.greenAccent,
+                padding: EdgeInsets.all(16.0),
+                child: RaisedButton(
+                  onPressed: () => debugPrint("Image selection"),
+                  color: Colors.purple,
+                  splashColor:  Colors.lightGreenAccent,
+                  child: Text("Select Image"),
+                )
+            ),
+
+          ],
+        )
     );
+
   }
 }
 
 
+
+//class _RegisterState extends State<Register> {
 //
-//
-//
-//
-//
-////clases para agregar receta
-//class RegisterRecipe extends MyApp {
-//
+//  final TextEditingController _userController = TextEditingController();
 //
 //  @override
 //  Widget build(BuildContext context) {
+//    return Scaffold(
 //
-////    return Container(
-////      child: Row(
-////      children: <Widget>[
-////          Text("AYY weeeyyyy!")
-////      ],
-////        )
-////      );
+//      appBar: AppBar(
+//        centerTitle: true,
+//        title: Text("Register your Recipe",
+//          style: TextStyle(
+//              color: Colors.white70,
+//              fontSize: 14.0
+//          ),
+//        ),
+//        backgroundColor: Colors.white30,
+//      ),
 //
-//
-//  return Container(
-//      alignment: Alignment.topCenter,
-//      child: Column(
-//          children: <Widget>[
-//            Image.asset("images/caos.jpg",
-//                height: 60.0,
-//                width: 60.0,
-//                color: Colors.purple
-//            ),
-//
+//      backgroundColor: Colors.white70,
+//      body: Container(
+//          alignment: Alignment.topCenter,
+//          child: Column(
+//              children: <Widget>[
 ////              Form
-//            Container(
-////                heght: 200.0,
-//                width: 320.0,
-//                color: Colors.white,
+//                Container(
+//                    height: 220.0,
+//                    width: 320.0,
+//                    padding: EdgeInsets.only(top:32.0),
+//                    color: Colors.black26,
+//                    child: Column(
+//                      mainAxisAlignment: MainAxisAlignment.,
+//                        children: <Widget>[
+//                          TextField(
+//                            autofocus: true,
+//                            controller: _userController,
+//                            decoration: InputDecoration(
+//                                hintText: "Recipe Name",
+//                                icon: Icon(Icons.fastfood),
+//                                fillColor: Colors.black26
+//                                )
+//                              ),
 //
-//                child: Column(
-//                    children: <Widget>[
-//                      TextField(
-//                        controller: _userController,
-//                        decoration: InputDecoration(
-//                            hintText: "User name", icon: Icon(Icons.person)
-//                        ),
-//                      )
-//                    ]
-//                )
+//                          TextField(
+//                              autofocus: false,
+//                              controller: _userController,
+//                              decoration: InputDecoration(
+//                                  hintText: "Recipe Ingredients",
+//                                  icon: Icon(Icons.fastfood),
+//                                  fillColor: Colors.black26
+//                              )
+//                          ),
 //
-//            ),
-//          ])
-//  ),
+//                          TextField(
+//                              autofocus: false,
+//                              controller: _userController,
+//                              decoration: InputDecoration(
+//                                  hintText: "Recipe Instructions",
+//                                  icon: Icon(Icons.fastfood),
+//                                  fillColor: Colors.black26
+//                              )
+//                          ),
 //
+////
+////                          new Center(
+////                            child:
+//                            Container(
+//                              margin: const EdgeInsets.all(10.0),
+//                              color: const Color(0xFF00FF00),
+//                              width: 88.0,
+//                              height: 48.0,
+//                            ),
+////                          )
+////
+//
+//                            ],
+//                          ),
+//                        )
+//                      ]
+//                    )
+//                  )
+//                );
 //
 //  }
 //}
